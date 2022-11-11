9fa27073cc09848749804088f0d3acd6ee349487
sarva <sarva4dim@yandex.ru>
2022-11-11 14:39:34 +0300

    Final

--- yatube/core/views.py
+++ yatube/core/views.py
@@ -3,17 +3,23 @@ from http import HTTPStatus
 
 
 def page_not_found(request, exception):
-    return render(request, 'core/404.html', {'path': request.path},
-                  HTTPStatus.NOT_FOUND)
+    return render(
+        request, 'core/404.html',
+        {'path': request.path},
+        status=HTTPStatus.NOT_FOUND
+    )
 
 
 def csrf_failure(request, reason=''):
-    return render(request, 'core/403csrf.html')
+    return render(request, 'core/403csrf.html', status=HTTPStatus.FORBIDDEN)
 
 
 def server_error(request):
-    return render(request, 'core/500.html', HTTPStatus.INTERNAL_SERVER_ERROR)
+    return render(
+        request, 'core/500.html',
+        status=HTTPStatus.INTERNAL_SERVER_ERROR
+    )
 
 
 def permission_denied(request, exception):
-    return render(request, 'core/403.html', HTTPStatus.FORBIDDEN)
+    return render(request, 'core/403.html', status=HTTPStatus.FORBIDDEN)
